1592b3b41d9ac1982d21c2351f7a9c1e6f8032eb
William Wold <wm@wmww.sh>
2020-07-27 13:31:15 -0400

    Remove whole out dir when cleaning

--- clean_engine.sh
+++ clean_engine.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 source ./env.sh
 
 echo "Nuking engine build dir"
-rm -Rf ./engine_gclient/src/out/host_debug_unopt/
+rm -Rf ./engine_gclient/src/out/
 cd ./engine_gclient/src/flutter
 
 echo "Generating new build dir"
