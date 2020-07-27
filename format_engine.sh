14cc76078dfc8a8e2fdebed01fd4493959146c56
William Wold <wm@wmww.sh>
2020-07-27 13:30:47 -0400

    Lint after formatting

--- format_engine.sh
+++ format_engine.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 source ./env.sh
 
 cd engine_gclient/src/flutter/
-./ci/format.sh | patch -p0
-cd ../../..
+./ci/format.sh | patch -p0 || echo "Did not format"
+cd ..
+./flutter/ci/lint.sh
