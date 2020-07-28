c41ac94b80ed93497e6bda50fead231befb092ed
William Wold <wm@wmww.sh>
2020-07-28 08:19:43 -0400

    Use --local-engine option

--- build_demoapp.sh
+++ build_demoapp.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 source ./env.sh
 
 cd demoapp
-flutter build linux --debug
+flutter build linux --debug --local-engine-src-path ../engine_gclient/src --local-engine=host_debug_unopt
--- create_demoapp.sh
+++ create_demoapp.sh
@@ -5,7 +5,7 @@ source ./env.sh
 
 if test ! -d demoapp; then
   echo "Creating demo app"
-  flutter create demoapp
+  flutter create --local-engine-src-path engine_gclient/src --local-engine=host_debug_unopt demoapp
 else
   echo "Demo app already exists"
   echo "If shit's broken, you may want to nuke the linux subdirectory, the build subdirectory or the whole thing"
