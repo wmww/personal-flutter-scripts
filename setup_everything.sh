95bc3ed75c7cbb51bc80f22fee4241fe41e4021d
William Wold <wm@wmww.sh>
2020-07-30 06:06:05 -0400

    Require cmake

--- setup_everything.sh
+++ setup_everything.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 source ./env.sh
 
-echo "If any of the following checks fail, run 'apt install build-essential clang ninja-build python2 curl'"
+echo "If any of the following checks fail, run 'apt install build-essential clang ninja-build python2 curl cmake'"
 git --version &>/dev/null
 gcc --version &>/dev/null
 clang --version &>/dev/null
 ninja --version &>/dev/null
 python2 --version &>/dev/null
 curl --version &>/dev/null
+cmake --version &>/dev/null
 
 echo "Pulling engine"
 ./pull_engine.sh
