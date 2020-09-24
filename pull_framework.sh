57dea7c3b987f7aee0b1e8731cdab5fbe140b5a9
William Wold <wm@wmww.sh>
2020-09-24 12:45:40 -0700

    Add robert remote to flutter repo

--- pull_framework.sh
+++ pull_framework.sh
@@ -6,6 +6,7 @@ source ./env.sh
 if test ! -d flutter; then
   echo "Cloning flutter framework"
   git clone https://github.com/flutter/flutter.git
+  git -C flutter remote add robert git@github.com:robert-ancell/flutter.git
 fi
 
 git -C flutter checkout master
