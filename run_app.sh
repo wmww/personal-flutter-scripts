0ab4845d8684ec9ce65135be2338c4f3d6e3f308
William Wold <wm@wmww.sh>
2021-01-28 16:27:58 -0800

    Improve use-python2.sh

--- run_app.sh
+++ run_app.sh
@@ -5,4 +5,5 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 APP_DIR="${1:-.}" # First command line argument or current directory if none
 source "$SOURCE_DIR/env.sh"
 BIN=$(find "$APP_DIR/build/linux/debug/bundle" -maxdepth 1 -type f -executable)
+echo "Running $BIN"
 "$BIN"
--- use-python2.sh
+++ use-python2.sh
@@ -1,6 +1,5 @@
 # Source this file to default to python2 in your local environment without changing the system
 mkdir -p /tmp/python2-default
-echo 'python2 $@' > /tmp/python2-default/python
-chmod +x /tmp/python2-default/python
+ln -sf $(which python2) /tmp/python2-default/python
 export PATH="/tmp/python2-default:${PATH:-}"
 echo "Using Python2 by default in this environment"
