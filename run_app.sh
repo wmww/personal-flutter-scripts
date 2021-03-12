e9759d2b6099c872b2b1d001af4686bf9b0837af
William Wold <wm@wmww.sh>
2021-03-12 13:49:53 -0800

    Fix run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-set -euo pipefail
+set -eu
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-APP_DIR="${1:-.}" # First command line argument or current directory if none
 source "$SOURCE_DIR/env.sh"
-BIN=$(find "$APP_DIR/build/linux/debug/bundle" -maxdepth 1 -type f -executable)
-echo "Running $BIN"
-"$BIN"
+
+APP_DIR="${1:-.}" # First command line argument or current directory if none
+BUILD_DIR="$APP_DIR/build"
+ls "$BUILD_DIR" > /dev/null # make sure it exists
+
+BIN=$(find "$BUILD_DIR" -type f -executable | grep bundle | cat) # trailing cat keeps the script from exiting if there's an error
+
+if test -z "$BIN"; then
+  echo "Failed to find binary in $BUILD_DIR"
+  exit 1
+elif test $(echo "$BIN" | wc -l) -ne 1; then
+  echo "Found multiple binaries:"
+  echo "$BIN"
+  exit 1
+else
+  echo "Running $BIN"
+  "$BIN"
+fi
