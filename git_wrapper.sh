09488468ae1c5fa2bf0843c04fcc9d5b6abc2dac
William Wold <wm@wmww.sh>
2021-03-16 09:36:17 -0700

    Fixed git wrapper script and added logging to it

--- git_wrapper.sh
+++ git_wrapper.sh
@@ -3,6 +3,12 @@
 # Wrapper script for git that automatically converts between default branch names (master, main, trunk)
 # This is required because (at the time of writing, google repos keep changing)
 
+if test -z "$GIT_WRAPPER_LOG"; then
+  GIT_WRAPPER_LOG="/dev/null"
+fi
+
+echo "$(date): Invoked with: $@" >> "$GIT_WRAPPER_LOG"
+
 # We can't just invoke `git` because we are git
 if test -z "$GIT_BIN"; then
   echo '$GIT_BIN not set, needs to be for git compatibility script to work'
@@ -26,7 +32,11 @@ for (( i=0; i<"$LEN"; i++ )); do
   # Only works for repos with remotes
   BRANCH=$("$GIT_BIN" "${GIT_ARGS[@]}" symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
   GIT_NOT_NAME='$\|^\|[^A-Za-z_\-]' # Regex that matches anything that's not part of a git branch name
-  ARGS[$i]=$(echo "${ARGS[$i]}" | sed -e "s#\($GIT_NOT_NAME\)\(master\|main\|trunk\)\($GIT_NOT_NAME\)#\1$BRANCH\3#g")
+  ORIGINAL="${ARGS[$i]}"
+  ARGS[$i]=$(printf '%s' "$ORIGINAL" | sed -e "s#\($GIT_NOT_NAME\)\(master\|main\|trunk\)\($GIT_NOT_NAME\)#\1$BRANCH\3#g")
+  if test "$ORIGINAL" != "${ARGS[$i]}"; then
+    echo "$(date):     Transformed: '$ORIGINAL' -> '${ARGS[$i]}'" >> "$GIT_WRAPPER_LOG"
+  fi
 done
 
 "$GIT_BIN" "${ARGS[@]}"
